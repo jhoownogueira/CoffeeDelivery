7777f776cde6019e2eb0d57159d9b13b70a74b59
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-11-07 17:25:50 -0300

    Start Page Checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,11 +1,70 @@
+import { CurrencyDollar, MapPinLine } from "phosphor-react";
 import { CheckoutContainer } from "./styles";
 
 export function Checkout() {
   return (
     <CheckoutContainer>
       <div className="grid">
-        <div className="left_container"></div>
-        <div className="right_container"></div>
+        <div className="left_container">
+          <h4>Complete seu pedido</h4>
+          <div className="container">
+            <div className="container_top">
+              <MapPinLine size={22} />
+              <div className="top_info">
+                <span>Endereço de Entrega</span>
+                <p>Informe o endereço onde deseja receber seu pedido</p>
+              </div>
+            </div>
+            <div className="container_inputs">
+              <div className="field">
+                <input type="text" id="cep" placeholder="CEP" />
+              </div>
+              <div className="field">
+                <input type="text" id="rua" placeholder="RUA" />
+              </div>
+              <div className="field_group">
+                <div className="field">
+                  <input type="text" id="numero" placeholder="Número" />
+                </div>
+                <div className="field">
+                  <input
+                    type="text"
+                    id="complemento"
+                    placeholder="Complemento"
+                  />
+                  <label htmlFor="complemento">Opcional</label>
+                </div>
+              </div>
+              <div className="field_group">
+                <div className="field">
+                  <input type="text" id="bairro" placeholder="Bairro" />
+                </div>
+                <div className="field">
+                  <input type="text" id="cidade" placeholder="Cidade" />
+                </div>
+                <div className="field">
+                  <input type="text" id="uf" placeholder="UF" />
+                </div>
+              </div>
+            </div>
+            <div className="container_top secondary_color">
+              <CurrencyDollar size={22} />
+              <div className="top_info">
+                <span>Pagamento</span>
+                <p>
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </p>
+              </div>
+            </div>
+            <div className="container_payment">
+              <input type="radio" />
+            </div>
+          </div>
+        </div>
+        <div className="right_container">
+          <h4>Cafés selecionados</h4>
+        </div>
       </div>
     </CheckoutContainer>
   );
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,5 +1,137 @@
 import styled from "styled-components";
 
-export const CheckoutContainer = styled.section`
+export const CheckoutContainer = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 6rem 0 0;
 
-`
+  .grid {
+    width: 100%;
+    max-width: 72rem;
+    padding: 0 1rem;
+    display: flex;
+
+    .left_container {
+      width: 100%;
+      max-width: 40rem;
+
+      .container {
+        width: 100%;
+        padding: 2.5rem;
+
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+
+        .container_top {
+          width: 100%;
+          display: flex;
+          align-items: flex-start;
+
+          svg {
+            color: ${(props) => props.theme["primary-color-dark"]};
+          }
+
+          .top_info {
+            line-height: 130%;
+
+            span {
+              color: ${(props) => props.theme["base-text"]};
+            }
+
+            p {
+              font-size: 0.875rem;
+              color: ${(props) => props.theme["base-subtitle"]};
+            }
+          }
+
+          &.secondary_color {
+            svg {
+              color: ${(props) => props.theme["secondary-color"]};
+            }
+          }
+        }
+
+        .container_inputs {
+          width: 100%;
+          display: flex;
+          gap: 1rem;
+          flex-direction: column;
+          align-items: flex-start;
+
+          margin-top: 2rem;
+          margin-bottom: 3.25rem;
+
+          .field {
+            position: relative;
+            width: 100%;
+
+            #cep {
+              width: 50%;
+            }
+
+            input {
+              width: 100%;
+              height: 2.625rem;
+              border-radius: 4px;
+              padding: 0.75rem;
+              border: 1px solid ${(props) => props.theme["base-button"]};
+              background: ${(props) => props.theme["base-input"]};
+            }
+
+            label {
+              position: absolute;
+              top: 0.875rem;
+              right: 1rem;
+
+              font-style: italic;
+              font-size: 0.75rem;
+              line-height: 130%;
+              color: ${(props) => props.theme["base-label"]};
+            }
+          }
+
+          .field_group {
+            width: 100%;
+            display: flex;
+            gap: 0.75rem;
+
+            #complemento {
+              padding-right: 4.2rem;
+            }
+          }
+        }
+
+        .container_payment {
+          input[type="radio"] {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 0.75rem;
+            padding: 1rem;
+            border: 1px solid transparent;
+            border-radius: 6px;
+            color: ${(props) => props.theme["base-text"]};
+            background: ${(props) => props.theme["base-button"]};
+
+            svg {
+              color: ${(props) => props.theme["secondary-color"]};
+            }
+
+            &:hover {
+              background: ${(props) => props.theme["base-hover"]};
+            }
+          }
+        }
+      }
+    }
+
+    .right_container {
+      width: 100%;
+      max-width: 28rem;
+    }
+  }
+`;
